91ca0cc8a940f9c1cddf2f0c6b724916f1c42993
mr-tajul-Islam <tajul.azazil@gmail.com>
2025-05-08 09:08:35 +0600

    ok

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,45 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "HandsOn",
+           "title": "HandsOn - Comprehensive Online Education",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://images.ggtasker.co.uk/wp-content/uploads/sites/4/2024/10/nine-app-logo.png",
+           "priceRange": "$$$",
+           "image": "https://images.ggtasker.co.uk/wp-content/uploads/sites/4/2024/10/nine-social.png",
+           "description": "HandsOn offers comprehensive online education. Access a wide range of courses and resources to enhance your learning experience.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+var currentDate = new Date(); var monthNamesShort = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']; var month = monthNamesShort[currentDate.getMonth()]; document.write(month);
+var currentDate = new Date(); var day = currentDate.getDate(); document.write(day);
+var currentDate = new Date(); var monthNamesShort = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']; var month = monthNamesShort[currentDate.getMonth()]; document.write(month);
+var currentDate = new Date(); var day = currentDate.getDate(); document.write(day);
+var currentDate = new Date(); var monthNamesShort = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']; var month = monthNamesShort[currentDate.getMonth()]; document.write(month);
+var currentDate = new Date(); var day = currentDate.getDate(); document.write(day);
+document.write(new Date().getFullYear());
+Value for custom_js_code
